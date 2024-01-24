55f3f0b8d05b5d2dbd4cf9a488e25ecaa692d1f2
veronbungag <rveron.bungag@gmail.com>
2024-01-24 23:13:32 +0800

    for loop

--- printing_a_downward_half_pyramid_pattern_of_star.py
+++ printing_a_downward_half_pyramid_pattern_of_star.py
@@ -1,6 +1,8 @@
 #define a function for downward half pyramid pattern
 def downward_half_pyramid(n):
-#for loop range
+    #for loop range
+    for i in range(n, 0, -1):
+        for j in range(i):
 #print a star for each column
 #print a newline
 #check if working add number in function
